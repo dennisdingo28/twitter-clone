35b1c794435c959d463afaea60cf1e50ea8472ac
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-07-28 21:03:57 +0300

    tweet likes component

--- components/TweetInteractions.tsx
+++ components/TweetInteractions.tsx
@@ -1,12 +1,10 @@
 "use client"
-import { MessageSquare, Share, Heart, BarChart } from "lucide-react"
+import { MessageSquare, Share, BarChart } from "lucide-react"
 import IconInteraction from "./ui/IconInteraction"
-import { Tweet, User as UserClient } from "@prisma/client"
-import { useEffect, useState } from "react"
 import { User } from "next-auth"
-import { useMutation } from "@tanstack/react-query"
-import likeTweet from "@/lib/likeTweet"
-import { useRouter } from "next/navigation"
+import TweetLikes from "./TweetLikes";
+import { Tweet, User as UserClient } from "@prisma/client"
+
 
 interface TweetInteractionsProps {
   tweet: Tweet & {
@@ -16,50 +14,16 @@ interface TweetInteractionsProps {
 }
 
 const TweetInteractions:React.FC<TweetInteractionsProps> = ({tweet,user}) => {
-  const router = useRouter();
+  
 
-  //like tweet
-  const [tweetLikes,setTweetLikes] = useState<Array<string>>(tweet.likes);  
-  const userLiked = tweetLikes.some((userId)=>userId===user?.id);
-
-  useEffect(()=>{
-    handleTweetLike();
-  },[tweetLikes]);
-
-  const {mutate:handleTweetLike, isLoading} = useMutation({
-    mutationFn:async ()=>{
-      await likeTweet(tweet.id,tweetLikes);
-      return null;
-    },
-    onSuccess:()=>{
-      router.refresh();
-    },
-    onError: ()=>{
-      const filteredLikes = tweetLikes.filter(userId=>userId!==user?.id);
-      setTweetLikes(filteredLikes);
-    }
-  });
-
+  
   //end like tweet
 
   return (
     <div className="flex items-center justify-between">
         <IconInteraction className="group-hover:text-lightBlue duration-150" handleClick={()=>{}} textData="4,757" icon={<MessageSquare size={40} className="text-[#9CA093] group-hover:text-lightBlue group-hover:bg-[#0C2839] rounded-full p-2 duration-150"/>}/>
         <IconInteraction className="group-hover:text-[#00BA7C] duration-150" handleClick={()=>{}} textData="4,125" icon={<Share size={40} className="text-[#9CA093] group-hover:text-[#00BA7C] group-hover:bg-[#062E21] rounded-full p-2 duration-150"/>}/>
-        <IconInteraction className={`group-hover:text-[#F91880] duration-150 ${userLiked && "text-[#F91880]"} ${isLoading && "pointer-events-none"}`} handleClick={()=>{
-          if(userLiked){
-            const filteredLikes = tweetLikes.filter(userId=>userId!==user?.id);
-            setTweetLikes(filteredLikes);
-           
-          }else{
-            setTweetLikes(prev=>{
-              return [
-                ...prev,
-                String(user?.id)
-              ]
-            })
-          }
-        }} textData={String(tweetLikes.length)} icon={<Heart size={40} className={`text-[#9CA093] group-hover:text-[#F91880] group-hover:bg-[#200914] rounded-full p-2 duration-150 ${userLiked && "text-[#F91880]"}`}/>}/>
+        <TweetLikes tweet={tweet} user={user}/>
         <IconInteraction className="group-hover:text-lightBlue duration-150" handleClick={()=>{}} textData="7,632" icon={<BarChart size={40} className="text-[#9CA093] group-hover:text-lightBlue group-hover:bg-[#0C2839] rounded-full p-2 duration-150"/>}/>
     </div>
   )
---
+++ components/TweetLikes.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import IconInteraction from "./ui/IconInteraction";
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import { useMutation } from "@tanstack/react-query"
+import likeTweet from "@/lib/likeTweet";
+import { Tweet, User as UserClient } from "@prisma/client"
+import { User } from "next-auth"
+import { Heart } from "lucide-react";
+
+interface TweetLikeProps {
+    tweet: Tweet & {
+      user: UserClient;
+    };
+    user: User | undefined;
+  }
+const TweetLikes:React.FC<TweetLikeProps> = ({tweet,user}) => {
+    const router = useRouter();
+
+  //like tweet
+  const [tweetLikes,setTweetLikes] = useState<Array<string>>(tweet.likes);  
+  const userLiked = tweetLikes.some((userId)=>userId===user?.id);
+  
+  useEffect(()=>{
+    handleTweetLike();
+  },[tweetLikes]);
+
+  const {mutate:handleTweetLike, isLoading} = useMutation({
+    mutationFn:async ()=>{
+      await likeTweet(tweet.id,tweetLikes);
+      return null;
+    },
+    onSuccess:()=>{
+      router.refresh();
+    },
+    onError: ()=>{
+      const filteredLikes = tweetLikes.filter(userId=>userId!==user?.id);
+      setTweetLikes(filteredLikes);
+    }
+  });
+
+  return (
+    <>
+    <IconInteraction className={`group-hover:text-[#F91880] duration-150 ${userLiked && "text-[#F91880]"} ${isLoading && "pointer-events-none"}`} handleClick={()=>{
+          if(userLiked){
+            const filteredLikes = tweetLikes.filter(userId=>userId!==user?.id);
+            setTweetLikes(filteredLikes);
+           
+          }else{
+            setTweetLikes(prev=>{
+              return [
+                ...prev,
+                String(user?.id)
+              ]
+            })
+          }
+        }} textData={String(tweetLikes.length)} icon={<Heart size={40} className={`text-[#9CA093] group-hover:text-[#F91880] group-hover:bg-[#200914] rounded-full p-2 duration-150 ${userLiked && "text-[#F91880]"}`}/>}/>
+    </>
+  )
+}
+
+export default TweetLikes
