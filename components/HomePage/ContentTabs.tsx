bfe62f10d1fc8c471c872f12a3166859a01fd24d
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-08-02 12:26:24 +0300

    fixes

--- components/HomePage/ContentTabs.tsx
+++ components/HomePage/ContentTabs.tsx
@@ -7,10 +7,10 @@ const ContentTabs = () => {
     
     return (
         <div className="flex items-center justify-evenly border-b border-darkGray">
-            <div className="hover:bg-[rgba(24,24,24,.9)] cursor-pointer p-3 duration-150">
+            <div className="hover:bg-[rgba(24,24,24,.9)] cursor-pointer p-3 duration-150 flex justify-center w-full">
                 <Link href={"/"} ><p className={`${path==='/' ? "font-bold border-b-[3px] border-lightBlue text-white":"font-normal text-[#71767b]"}`}>For You</p></Link>
             </div>
-            <div className="hover:bg-[rgba(24,24,24,.9)] cursor-pointer p-3 duration-150">
+            <div className="hover:bg-[rgba(24,24,24,.9)] cursor-pointer p-3 duration-150 flex justify-center w-full">
                 <Link href={"/following"} ><p className={`${path==='/following' ? "font-bold border-b-[3px] border-lightBlue text-white":"font-normal text-[#71767b]"}`}>Following</p></Link>
             </div>
         </div>
