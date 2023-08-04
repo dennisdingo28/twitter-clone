0b9d99e72140ebc7aeef85aa99a338610cd0ac6e
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-08-04 11:37:13 +0300

    follwing tweets

--- app/following/page.tsx
+++ app/following/page.tsx
@@ -1,4 +1,3 @@
-import Content from "@/components/FollowingPage/Content"
 import ContentTabs from "@/components/HomePage/ContentTabs"
 import Header from "@/components/HomePage/Header"
 import Tweets from "@/components/Tweets"
@@ -6,6 +5,7 @@ import TweetsContainer from "@/components/TweetsContainer"
 import Paragraph from "@/components/ui/paragraph"
 import { getAuthSession } from "@/lib/authOptions"
 import prismadb from "@/lib/db"
+import { Comment, Tweet, User } from "@prisma/client"
 import Link from "next/link"
 import { Suspense } from "react"
 
@@ -17,6 +17,32 @@ const FollowingTweets = async () => {
         }
     });
     
+
+
+    const followersTweets = userFollowingUsers.map(async(follower)=>{
+        const userTweets = await prismadb.tweet.findMany({
+            where:{
+                userId:follower.followingUserId,
+            },
+            include:{
+                user:true,
+                comments:true,
+            }
+        });
+        console.log("ut",userTweets);
+        
+       return {
+        ...userTweets,
+       }
+    })
+    const tweets = await Promise.all(followersTweets);
+    const updatedTweets = [];
+
+    for(let i =0;i<tweets.length;i++){
+      updatedTweets.push(tweets[i][i]);
+    }
+    
+
   return (
     <div className="border-r border-darkGray flex-1">
       <div className="content">
@@ -27,7 +53,7 @@ const FollowingTweets = async () => {
         <Header/>
       </div>
       <Suspense fallback={<p className="text-center mt-4 font-bold text-[1.1em]">Loading the tweeets...</p>}>
-           <Content followers={userFollowingUsers}/>
+            <Tweets tweets={updatedTweets}/>
       </Suspense>
     </div>
   )
